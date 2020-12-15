ca1445ae86006c56dde4b0463a5e9f9e485a9286
20taxitaxi00 <takushi0210@icloud.com>
2020-12-15 20:48:26 +0900

    headerとfooter終了

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    // ハンバーガーメニュー
+    $('#hamburger-btn').on('click',function(){
+        let isActive = $(this).hasClass('on');
+        toggleDrawer(isActive);
+    });
+
+
+    // ドロワー
+    function toggleDrawer(isActive) {
+        $('.js-hamburger')
+        if (isActive) {
+        $('#drawer-bg').fadeOut(600);
+        } else {
+        $('#drawer-bg').fadeIn(600);
+        }
+        $('.js-hamburger').toggleClass('on');
+        $('.js-drawer').toggleClass('on');
+    }
+
+
+
+
+
+});
