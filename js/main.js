03f944b253328ec5a7bbc83a1127aefbd60760f0
20taxitaxi00 <takushi0210@icloud.com>
2020-12-16 19:12:47 +0900

    topbutton完成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(function(){
 
+    // トップに戻る
+    $('#return-top').on('click', function(){
+        $('html, body').animate({scrollTop:0}, 500);
+    });
+
     // ハンバーガーメニュー
     $('#hamburger-btn').on('click',function(){
         let isActive = $(this).hasClass('on');
