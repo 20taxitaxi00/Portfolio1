e2ffba98596246753226c459ef2a4359dfa70dc9
20taxitaxi00 <takushi0210@icloud.com>
2020-12-18 01:51:21 +0900

    ader部分終了

--- js/main.js
+++ js/main.js
@@ -23,45 +23,50 @@ $(function(){
     }
 
     // 写真のFadeIn & FadeOut
-    $(window).on('scroll', function(){
-        let main_logo = $('.main-logo').offset().top;
-        let wh = $(window).height();
-        let now_position = $(window).scrollTop();
+    // $(window).on('scroll', function(){
+    //     let main_logo = $('.main-logo').offset().top;
+    //     let wh = $(window).height();
+    //     let now_position = $(window).scrollTop();
 
-        if (main_logo <= wh + now_position){
-            $('.main-img').fadeIn(600);
-        } else {
-            $('.main-img').fadeOut(600);
-        }
-    });
+    //     if (main_logo <= wh + now_position){
+    //         $('.main-img').fadeIn(600);
+    //     } else {
+    //         $('.main-img').fadeOut(600);
+    //     }
+    // });
 
     // 記事のread-more下線
-	$('.shadow').on('mouseover', function(){
-		$(this).find('span').animate({'width':'100%'}, 500)
-	});
-	$('.shadow').on('mouseleave', function(){
-		$(this).find('span').animate({'width':'30%'}, 500)
+    $('.shadow').on('mouseover', function(){
+      $(this).find('span').animate({'width':'100%'}, 500)
     });
+    $('.shadow').on('mouseleave', function(){
+      $(this).find('span').animate({'width':'30%'}, 500)
+      });
     
     // 最初にロゴがfade
     setTimeout(function(){
 		$('.logo-fade p').fadeIn(1000);
-	},500);
-	setTimeout(function(){
+    },500);
+    setTimeout(function(){
 		$('.logo-fade').fadeOut(800);
     },2500);
 
     // スライダー
-	// let swipeOption = {
-	// 	loop: true,
-	// 	effect: 'fade',
-	// 	autoplay: {
-	// 	  delay: 3000,
-	// 	  disableOnInteraction: false,
-	// 	},
-	// 	speed: 1500,
-	// }
-	// new Swiper('.swiper-container', swipeOption);
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        slidesPerView: 2,
+        spaceBetween: 0,
+        centeredSlides : true,
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        breakpoints: {
+          767: {
+            slidesPerView: 1,
+            spaceBetween: 0
+          }
+        }
+      })
 
 
 });
